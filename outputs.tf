f5277f4c026701b68bae40072c57cb7608572d97
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:15:26 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,16 @@ output "storage_account_static_websites" {
   description = "All storage_account_static_website resources"
   value       = azurerm_storage_account_static_website.storage_account_static_websites
 }
+output "storage_account_static_websites_error_404_document" {
+  description = "List of error_404_document values across all storage_account_static_websites"
+  value       = [for k, v in azurerm_storage_account_static_website.storage_account_static_websites : v.error_404_document]
+}
+output "storage_account_static_websites_index_document" {
+  description = "List of index_document values across all storage_account_static_websites"
+  value       = [for k, v in azurerm_storage_account_static_website.storage_account_static_websites : v.index_document]
+}
+output "storage_account_static_websites_storage_account_id" {
+  description = "List of storage_account_id values across all storage_account_static_websites"
+  value       = [for k, v in azurerm_storage_account_static_website.storage_account_static_websites : v.storage_account_id]
+}
 
