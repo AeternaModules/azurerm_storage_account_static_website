0c2d9cddcbfbb741c7c77b27d199828f229087a3
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:32:02 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "storage_account_static_websites_id" {
+  description = "Map of id values across all storage_account_static_websites, keyed the same as var.storage_account_static_websites"
+  value       = { for k, v in azurerm_storage_account_static_website.storage_account_static_websites : k => v.id }
+}
 output "storage_account_static_websites_error_404_document" {
   description = "Map of error_404_document values across all storage_account_static_websites, keyed the same as var.storage_account_static_websites"
   value       = { for k, v in azurerm_storage_account_static_website.storage_account_static_websites : k => v.error_404_document }
