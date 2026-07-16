7baa5ad7262399584e11a3519446d8dda62ac9f9
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:51:17 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,17 @@
 output "storage_account_static_websites_id" {
   description = "Map of id values across all storage_account_static_websites, keyed the same as var.storage_account_static_websites"
-  value       = { for k, v in azurerm_storage_account_static_website.storage_account_static_websites : k => v.id }
+  value       = { for k, v in azurerm_storage_account_static_website.storage_account_static_websites : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "storage_account_static_websites_error_404_document" {
   description = "Map of error_404_document values across all storage_account_static_websites, keyed the same as var.storage_account_static_websites"
-  value       = { for k, v in azurerm_storage_account_static_website.storage_account_static_websites : k => v.error_404_document }
+  value       = { for k, v in azurerm_storage_account_static_website.storage_account_static_websites : k => v.error_404_document if v.error_404_document != null && length(v.error_404_document) > 0 }
 }
 output "storage_account_static_websites_index_document" {
   description = "Map of index_document values across all storage_account_static_websites, keyed the same as var.storage_account_static_websites"
-  value       = { for k, v in azurerm_storage_account_static_website.storage_account_static_websites : k => v.index_document }
+  value       = { for k, v in azurerm_storage_account_static_website.storage_account_static_websites : k => v.index_document if v.index_document != null && length(v.index_document) > 0 }
 }
 output "storage_account_static_websites_storage_account_id" {
   description = "Map of storage_account_id values across all storage_account_static_websites, keyed the same as var.storage_account_static_websites"
-  value       = { for k, v in azurerm_storage_account_static_website.storage_account_static_websites : k => v.storage_account_id }
+  value       = { for k, v in azurerm_storage_account_static_website.storage_account_static_websites : k => v.storage_account_id if v.storage_account_id != null && length(v.storage_account_id) > 0 }
 }
 
